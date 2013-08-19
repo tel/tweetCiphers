97fe562e9d51bdf840e0bb1147e34b57a144e7ef
Joseph Abrahamson <me@jspha.com>
2013-08-18 21:46:32 -0400

    Functional step1?

--- art/TweetCipher.c
+++ art/TweetCipher.c
@@ -1,28 +1,74 @@
 #include <stdint.h> 
 #include <stdio.h> 
-#define LOOP(n) for(i=0;i<n;++i) 
-/* #define W(v,n) ((uint64_t*)v)[n] */
+#define LOOP(n) for(i=0;i<n;++i)
+#define W(v,n) ((uint64_t*)v)[n]
 #define R(v,n)(((v)<<(64-n))|((v)>>n)) 
+
+/* This forms the core of the random permutation function of the
+   sponge construction. I think it's supposed to be Salsa20, but we'll
+   check that later. In any case, `ROUNDS` is called every time the
+   permutation is to be performed in the sponge construction. */
 #define AXR(a,b,c,r) x[a]+=x[b];x[c]=R(x[c]^x[a],r); 
 #define G(a,b,c,d) {AXR(a,b,d,32) AXR(c,d,b,25) AXR(a,b,d,16) AXR(c,d,b,11)} 
+
+/* This loop is quite annoying, @(r--)@ decrements @r@ and returns its
+   previous value. If that value is zero then the loop
+   terminates. This means that the loop executes 6 times and observes
+   @r@ in @[5,4,3,2,1,0]@. */
 #define ROUNDS {for(r=6;r--;){LOOP(4) G(i,i+4,i+8,i+12) \
                               LOOP(4) G(i,(i+1)%4+4,(i+2)%4+8,(i+3)%4+12)}}
  
-int main(int _,char**v){ 
+
+int main(int _,char**v){
+
+  /* Less than classy initialization here. In particular it's cleverly
+     defining `f = true` if it's in encrypt mode and false
+     otherwise. */
   uint64_t x[16],i,c,r,f='e'==*v[1]; 
+
+  /* This initializes the sponge state to 16 multiples of the 8-byte
+     string "twithasi". According to the Keccak/Sponge site it's
+     naturally intiailized with just zeros, so this may not be
+     necessary. */
   LOOP(16)
     x[i]=i*0x7477697468617369ULL; 
-  LOOP(4) x[i]=((uint64_t*)v[2])[i];
-  LOOP(2) x[i+4]=((uint64_t*)v[3])[i];
+
+  /* Inserts (key || nonce) into the sponge state as the first 48
+     bytes and permutes. */
+  LOOP(4) x[i]=W(v[2],i);
+  LOOP(2) x[i+4]=W(v[3],i);
   ROUNDS;
+
+  /* The duplex steps! For each byte read in encode or decode it by
+     XORing with the first byte of the first uint64 of the sponge
+     state. */
   while((c=getchar())!=EOF){
+    /* If we're decrypting and we see a newline we're... done?
+       Really? */
     if(!f&&10==(x[0]^c)%256)return 0;
+    /* x[0] is a uint64 so (x[0] ^ c) upcasts the input character to
+       be a uint64. Then putchar truncates it. Endianness here is
+       problematic. Or does it matter or long as it's consistent? (At
+       the very least I'd think that a client with a different
+       endianness couldn't decrypt with this code). */
     putchar(x[0]^c);
+
+    /* This is a bizarre line---IF we're decrypting then zero out the
+       first byte of x[0]. Otherwise just leave it. How does that
+       work? */
     x[0]=c^(f?x[0]:x[0]&~255ULL);
+
+    /* Permute again at the end of each round. */
     ROUNDS;
   }
-  x[0]^=1; 
+
+  /* Truly no idea why this happens. Should it be a 10? It feels like
+     it's terminating the message on a 1. */
+  x[0]^=1;
   ROUNDS;
+
+  /* And this just prints out (x[4] XOR x[5]) and (x[6] XOR x[7]). Is
+     this the authentication? */
   LOOP(8) putchar(255&((x[4]^x[5])>>8*i)); 
   LOOP(8) putchar(255&((x[6]^x[7])>>8*i)); 
   return 0;
--- src/TweetCipher.hs
+++ src/TweetCipher.hs
@@ -1,4 +1,4 @@
-
+{-# LANGUAGE OverloadedStrings #-}
 -- |
 -- Module      : TweetCipher
 -- Copyright   : (c) Joseph Abrahamson 2013
@@ -16,16 +16,13 @@
 -- 
 -- <<http://blog.cryptographyengineering.com/2013/07/tweetnacl.html>>
 -- 
--- Unfortunately for non-usable crypto code, it's still written in
--- C. In order to explore the code in more detail and see the
--- algorithm laid out at a higher level this is the same algorithm
--- translated to beautiful Haskell.
-
 module TweetCipher where
 
 import System.Posix.Env.ByteString
 
+import Control.Lens
 import Control.Proxy
+import Control.Proxy.Trans.Writer
 import Control.Applicative
 import Control.Monad
 import Control.Monad.Primitive
@@ -33,6 +30,7 @@ import Control.Monad.Primitive
 import qualified Data.ByteString.Lazy as S
 import qualified Data.ByteString.Lazy.Internal as SI
 import qualified Data.ByteString as St
+import qualified Data.ByteString.Char8 as St8
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as M
 import Data.Vector.Generic.New
@@ -40,22 +38,14 @@ import Data.Binary.Get
 import Data.Bits
 import Data.Word
 
--- #define LOOP(n) for(i=0;i<n;++i)
-
 -- | Side-effecting loop
 loop :: (Monad m) => Int -> (Int -> m b) -> m ()
 loop n = forM_ [0..(pred n)]
-
--- #define W(v,n) ((uint64_t*)v)[n]
---
--- interpret v as a [Word64] and look for the nth
--- entry. 'lookupNth :: a -> n -> Word64'
 
 -- | #define R(v,n)(((v)<<(64-n))|((v)>>n))
 rf :: Bits a => a -> Int -> a
 rf v n = unsafeShiftL v (64 - n)  .|. unsafeShiftR v n
 
--- #define AXR(a,b,c,r) x[a] += x[b] ; x[c] = R(x[c]^x[a],r);
 axrf :: M.IOVector Word64 -> Int -> Int -> Int -> Int -> IO ()
 axrf x a b c r = do xb <- M.unsafeRead x b
                     xa <- M.unsafeRead x a
@@ -64,21 +54,12 @@ axrf x a b c r = do xb <- M.unsafeRead x b
                     xc <- M.unsafeRead x c
                     M.unsafeWrite x c (rf (xc `xor` new_xa) r)
 
--- #define G(a,b,c,d) {AXR(a,b,d,32) AXR(c,d,b,25) AXR(a,b,d,16) AXR(c,d,b,11)} 
 gf :: M.IOVector Word64 -> Int -> Int -> Int -> Int -> IO ()
 gf x a b c d = sequence_ [ axrf x a b d 32
                          , axrf x c d b 25
                          , axrf x a b d 16
                          , axrf x c d b 11
                          ]
-
--- This loop is quite annoying, @(r--)@ decrements @r@ and returns its
--- previous value. If that value is zero then the loop
--- terminates. This means that the loop executes 6 times and observes
--- @r@ in @[5,4,3,2,1,0]@.
--- 
--- #define ROUNDS {for(r=6;r--;){LOOP(4) G(i,i+4,i+8,i+12) \
---                               LOOP(4) G(i,(i+1)%4+4,(i+2)%4+8,(i+3)%4+12)}}
 
 roundsf :: M.IOVector Word64 -> IO ()
 roundsf x = forM_ [5,4..0] $ \r -> do
@@ -93,14 +74,12 @@ roundsf x = forM_ [5,4..0] $ \r -> do
 get64s :: Int -> S.ByteString -> V.Vector Word64
 get64s n = V.fromListN n . runGet (replicateM n getWord64host)
 
-writeTo
-  :: (PrimMonad m, M.Unbox a) =>
-     M.MVector (PrimState m) a -> Int -> V.Vector a -> m [()]
-writeTo m di v = forM [0..V.length v] $ \i -> do
+writeTo :: M.IOVector Word64 -> Int -> V.Vector Word64 -> IO ()
+writeTo m di v = forM_ [0..V.length v] $ \i -> do
   a <- V.unsafeIndexM v i
   M.unsafeWrite m (i + di) a
 
-contents :: Proxy p => r -> p a' a a1 St.ByteString IO r
+contents :: Proxy p => r -> Producer p St.ByteString IO r
 contents r = runIdentityP $ do
   bs <- lift $ S.getContents
   SI.foldrChunks (\e a -> respond e >> a) (return r) bs
@@ -108,53 +87,85 @@ contents r = runIdentityP $ do
 printer :: Proxy p => () -> p () St.ByteString b' b IO r
 printer = runIdentityK $ foreverK $ \_ -> request () >>= lift . St.putStr
 
-word8s
-  :: Proxy p =>
-     (Word8 -> IO Word8) -> () -> Pipe p St.ByteString St.ByteString IO ()
+word8s :: Proxy p => (Word8 -> IO Word8) ->
+          () -> Pipe p St.ByteString St.ByteString IO ()
 word8s f = mapMD bsMapM where
   bsMapM = fmap St.pack . St.foldl' (\a w -> (:) <$> f w <*> a) (return [])
 
+type Key   = V.Vector Word64
+type Nonce = V.Vector Word64
+
+-- | Initialize the sponge state using a repeated initializer
+-- ("twithasi" as a 'Word64'), copy in the key and nonce as the first
+-- 48 bytes, then shuffle using 5 'roundsf'.
+-- -------------------------------------------------------------------
+-- Note that the sponge state is only 1024 bits unlike the SHA3-Keccak
+-- itself which uses 1600. What is the capacity? (16*64 - 8 bits) What
+-- is the bitrate? (1 8-bit char) What is the padding rule? (Only
+-- accept character-sized inputs) What is the permutation function?
+-- (ROUNDS)
+--
+--     In the hermetic sponge strategy, the capacity determines the
+--     claimed level of security, and one can trade claimed security
+--     for speed by increasing the capacity c and decreasing the
+--     bitrate r accordingly, or vice-versa. When a padding rule is
+--     used with particular properties, one can securely instantiate
+--     sponge functions with different rates with the same
+--     fixed-length permutation. The simplest padding rule satisfying
+--     these properties is called the multi-rate padding: it appends a
+--     single 1-bit, then a variable number of zeroes and finally
+--     another 1-bit.
+--
+-- <<http://sponge.noekeon.org/>>
+-- -------------------------------------------------------------------
+initState :: Key -> Nonce -> IO (M.IOVector Word64)
+initState key nonce = do
+  x <- V.thaw $ V.generate 16 ((* 0x7477697468617369) . fromIntegral)
+  writeTo x 0 key
+  writeTo x 4 nonce
+  roundsf x
+  return x
+
+-- | Lens to peer at the first byte of a Word64.
+_Byte1 :: Lens' Word64 Word8
+_Byte1 = lens (\w      -> fromIntegral (w .&. 255))
+              (\w head -> (w .&. (complement 255)) .|. (fromIntegral head))
+
+-- | duplex
+-- -------------------------------------------------------------------
+-- <<http://sponge.noekeon.org/SpongeDuplex.pdf>>
+-- -------------------------------------------------------------------
+duplex :: Proxy p =>
+          Bool -> Key -> Nonce ->
+          () -> Pipe p St.ByteString St.ByteString IO () 
+duplex isEncrypt key nonce () = runIdentityP $ do
+  state <- lift $ initState key nonce
+  word8s (eachWord state) >=> terminate state $ ()
+  where
+    eachWord state w = do
+      x0 <- M.unsafeRead state 0
+      M.unsafeWrite state 1
+        $ xor (fromIntegral w)
+        $ if isEncrypt
+          then x0
+          else x0 & _Byte1 .~ 0
+      roundsf state
+      return $ x0 ^. _Byte1 . to (xor w)
+    terminate state () = return ()
+
+key, nonce :: V.Vector Word64
+key   = get64s 4 "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk"
+nonce = get64s 2 "vvvvvvvvvvvvvvvv"
+
 main :: IO ()
 main = do
-  (mode:key:nonce:_) <- getArgs
-  let isEncrypt = mode == "e"
-
-  -- * Initialize the sponge state
-  -- -----------------------------------------------------------------
-  -- Note that the sponge state is only 1024 bits unlike the
-  -- SHA3-Keccak itself which uses 1600. What is the capacity? (16*64
-  -- - 8 bits) What is the bitrate? (1 8-bit char) What is the padding
-  -- rule? (Only accept character-sized inputs) What is the
-  -- permutation function? (ROUNDS)
-  --
-  --     In the hermetic sponge strategy, the capacity determines the
-  --     claimed level of security, and one can trade claimed security
-  --     for speed by increasing the capacity c and decreasing the
-  --     bitrate r accordingly, or vice-versa. When a padding rule is
-  --     used with particular properties, one can securely instantiate
-  --     sponge functions with different rates with the same
-  --     fixed-length permutation. The simplest padding rule
-  --     satisfying these properties is called the multi-rate padding:
-  --     it appends a single 1-bit, then a variable number of zeroes
-  --     and finally another 1-bit.
-  --
-  -- <<http://sponge.noekeon.org/>>
-  -- -----------------------------------------------------------------
-
-  -- 0x7477697468617369 is "twithasi", but it could just be zero?
-  x <- V.thaw $ V.generate 16 ((* 0x7477697468617369) . fromIntegral)
-
-  -- Key and nonce reading
-  writeTo x 0 (get64s 4 (S.fromStrict key))
-  writeTo x 4 (get64s 2 (S.fromStrict nonce))
-
-  -- mix up the state
-  roundsf x
-
-  -- duplex
-  -- ----------------------------------------------
-  -- <<http://sponge.noekeon.org/SpongeDuplex.pdf>>
-  -- ----------------------------------------------
+  (mode:keyS:nonceS:_) <- getArgs
+  let key   = get64s 4 $ S.fromStrict keyS
+      nonce = get64s 2 $ S.fromStrict nonceS
+      isEncrypt = mode == "e"
+  runProxy $ contents
+             >-> duplex isEncrypt key nonce
+             >-> printer
 
   -- if decrypting, terminate on \n... haha!
   --
