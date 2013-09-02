78367cee69f403dc01b66b5026585eb807401eb0
Joseph Abrahamson <me@jspha.com>
2013-09-01 20:55:34 -0400

    * src/Salsa20.hs: Bang patterns for strict inner loops

--- src/Salsa20.hs
+++ src/Salsa20.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE BangPatterns #-}
 
 -- |
 -- Module      : Salsa20
@@ -12,7 +12,7 @@
 -- 
 -- Implementation of Salsa20.
 
-module Main where
+module Salsa20 where
 
 import Control.Applicative
 
@@ -26,7 +26,7 @@ import Data.Bits
 import Data.Bytes.Serial
 
 main :: IO ()
-main = salsa20 (Salsa 1 2 3 4 5 6 7 8 9 10 12 13 14 15 16)
+main = print . salsa20 $ Salsa 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
 
 salsa20 :: Salsa -> Salsa
 salsa20 s = (s ^+^)
@@ -51,8 +51,8 @@ instance Serial Salsa
 instance AdditiveGroup Salsa where
   zeroV = Salsa 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
   -- Writing this out so it's easier for the compiler to inline
-  (^+^) (Salsa a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 aa ab ac ad ae af)
-        (Salsa b0 b1 b2 b3 b4 b5 b6 b7 b8 b9 ba bb bc bd be bf) =
+  (^+^) !(Salsa a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 aa ab ac ad ae af)
+        !(Salsa b0 b1 b2 b3 b4 b5 b6 b7 b8 b9 ba bb bc bd be bf) =
     (Salsa (a0 + b0) (a1 + b1) (a2 + b2) (a3 + b3)
            (a4 + b4) (a5 + b5) (a6 + b6) (a7 + b7)
            (a8 + b8) (a9 + b9) (aa + ba) (ab + bb)
@@ -77,26 +77,26 @@ instance Serial Quarter
 
 quarter :: Quarter -> Quarter
 quarter (Quarter y0 y1 y2 y3) =
-  let z1 = xor y1 $ rotateL (y0 + y3) 7
-      z2 = xor y2 $ rotateL (z1 + y0) 9
-      z3 = xor y3 $ rotateL (z2 + z1) 13
-      z0 = xor y0 $ rotateL (z3 + z2) 18
+  let !z1 = xor y1 $ rotateL (y0 + y3) 7
+      !z2 = xor y2 $ rotateL (z1 + y0) 9
+      !z3 = xor y3 $ rotateL (z2 + z1) 13
+      !z0 = xor y0 $ rotateL (z3 + z2) 18
   in Quarter z0 z1 z2 z3
 
 row :: Salsa -> Salsa
 row (Salsa y0 y1 y2 y3 y4 y5 y6 y7 y8 y9 ya yb yc yd ye yf ) =
-  let Quarter z0 z1 z2 z3 = quarter $ Quarter y0 y1 y2 y3
-      Quarter z5 z6 z7 z4 = quarter $ Quarter y5 y6 y7 y4
-      Quarter za zb z8 z9 = quarter $ Quarter ya yb y8 y9
-      Quarter zf zc zd ze = quarter $ Quarter yf yc yd ye
+  let !(Quarter z0 z1 z2 z3) = quarter $ Quarter y0 y1 y2 y3
+      !(Quarter z5 z6 z7 z4) = quarter $ Quarter y5 y6 y7 y4
+      !(Quarter za zb z8 z9) = quarter $ Quarter ya yb y8 y9
+      !(Quarter zf zc zd ze) = quarter $ Quarter yf yc yd ye
   in Salsa z0 z1 z2 z3 z4 z5 z6 z7 z8 z9 za zb zc zd ze zf
 
 column :: Salsa -> Salsa
 column (Salsa x0 x1 x2 x3 x4 x5 x6 x7 x8 x9 xa xb xc xd xe xf) =
-  let Quarter y0 y4 y8 yc = quarter $ Quarter x0 x4 x8 xc
-      Quarter y5 y9 yd y1 = quarter $ Quarter x5 x9 xd x1
-      Quarter ya ye y2 y6 = quarter $ Quarter xa xe x2 x6
-      Quarter yf y3 y7 yb = quarter $ Quarter xf x3 x7 xb
+  let !(Quarter y0 y4 y8 yc) = quarter $ Quarter x0 x4 x8 xc
+      !(Quarter y5 y9 yd y1) = quarter $ Quarter x5 x9 xd x1
+      !(Quarter ya ye y2 y6) = quarter $ Quarter xa xe x2 x6
+      !(Quarter yf y3 y7 yb) = quarter $ Quarter xf x3 x7 xb
   in Salsa y0 y1 y2 y3 y4 y5 y6 y7 y8 y9 ya yb yc yd ye yf
 
 double :: Salsa -> Salsa
